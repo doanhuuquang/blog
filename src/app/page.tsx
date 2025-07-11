ee74619f13248f2b639598777711eb04c38221ad
Doan Huu Quang <doanhuuquang.dev@gmail.com>
2025-07-11 19:59:30 +0700

    deploy test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
-import Link from "next/link";
-import { type SanityDocument } from "next-sanity";
+// import Link from "next/link";
+// import { type SanityDocument } from "next-sanity";
 
-import { client } from "@/sanity/client";
+// import { client } from "@/sanity/client";
 import { PostCard } from "@/components/shared/post-card";
 import { Post } from "@/types/post";
 
-const POSTS_QUERY = `*[
-  _type == "post"
-  && defined(slug.current)
-]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`;
+// const POSTS_QUERY = `*[
+//   _type == "post"
+//   && defined(slug.current)
+// ]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`;
 
-const options = { next: { revalidate: 30 } };
+// const options = { next: { revalidate: 30 } };
 
 export default async function IndexPage() {
-  const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
+  // const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
   const post: Post = {
     id: "",
     title: "Sample Post Sample Post Sample Post Sample Post Sample Post",
