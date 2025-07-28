9c48ca366510afb23ac9b2d367948045f3b0b17b
Doan Huu Quang <doanhuuquang.dev@gmail.com>
2025-07-28 17:05:56 +0700

    Commit

--- src/components/shared/app-menu-mobile.tsx
+++ src/components/shared/app-menu-mobile.tsx
@@ -39,7 +39,6 @@ export function AppMenuMobile() {
             <DrawerDescription>
               Chào mừng bạn đến với Blog cá nhân của mình.
             </DrawerDescription>
-            {/* <SocialLinks className="items-center justify-center mt-4" /> */}
           </DrawerHeader>
           <div className="p-4 space-y-4">
             <div className="flex flex-col items-center justify-center gap-3">
